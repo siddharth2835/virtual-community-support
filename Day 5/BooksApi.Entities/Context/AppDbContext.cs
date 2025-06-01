98181c957c60451c28e25484bbec9d1688d8e489
siddharth2835 <153356253+siddharth2835@users.noreply.github.com>
2025-06-01 16:14:57 +0530

    Add files via upload

---
+++ Day 5/BooksApi.Entities/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BooksApi.Entities.Entities;
+
+namespace BooksApi.Entities
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; } // This maps to the Users table
+    }
+}
