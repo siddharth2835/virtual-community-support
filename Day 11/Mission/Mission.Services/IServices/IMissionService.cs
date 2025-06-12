fac6a0e7e82e4dbca602ed25f4fd7c6e77867522
siddharth2835 <153356253+siddharth2835@users.noreply.github.com>
2025-06-12 16:29:28 +0530

    Added files

---
+++ Day 11/Mission/Mission.Services/IServices/IMissionService.cs
@@ -0,0 +1,14 @@
+﻿using Mission.Entities.Models;
+
+namespace Mission.Services.IServices
+{
+    public interface IMissionService
+    {
+        Task<List<MissionRequestViewModel>> GetAllMissionAsync();
+        Task<MissionRequestViewModel?> GetMissionById(int id);
+        Task<bool> AddMission(MissionRequestViewModel model);
+        Task<bool> UpdateMission(MissionRequestViewModel model);
+        Task<bool> DeleteMission(int missionId);
+        Task<List<MissionDetailResponseModel>> GetClientSideMissionList();
+    }
+}
---
+++ Day 11/Mission/Mission/Controllers/MissionController.cs
@@ -0,0 +1,64 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission.Entities;
+using Mission.Entities.Models;
+using Mission.Services.IServices;
+
+namespace Mission.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MissionController(IMissionService missionService) : ControllerBase
+    {
+        private readonly IMissionService _missionService = missionService;
+
+        [HttpPost]
+        [Route("AddMission")]
+        public IActionResult AddMission(MissionRequestViewModel model)
+        {
+            var response = _missionService.AddMission(model);
+            return Ok(new ResponseResult() { Data = response, Result = ResponseStatus.Success, Message = "" });
+        }
+
+        [HttpGet]
+        [Route("MissionList")]
+        public async Task<IActionResult> GetAllMissionAsync()
+        {
+            var response = await _missionService.GetAllMissionAsync();
+            return Ok(new ResponseResult() { Data = response, Result = ResponseStatus.Success, Message = "" });
+        }
+
+        // Mission\Controllers\MissionController.cs
+        [HttpPost]
+        [Route("UpdateMission")]
+        public async Task<IActionResult> UpdateMission(MissionRequestViewModel model)
+        {
+            var success = await _missionService.UpdateMission(model);
+            if (success)
+            {
+                return Ok(new ResponseResult() { Data = null, Result = ResponseStatus.Success, Message = "Mission updated successfully." });
+            }
+            return NotFound(new ResponseResult() { Data = null, Result = ResponseStatus.Error, Message = "Mission not found." });
+        }
+
+        [HttpDelete]
+        [Route("DeleteMission/{id:int}")]
+        public async Task<IActionResult> DeleteMission(int id)
+        {
+            var success = await _missionService.DeleteMission(id);
+            if (success)
+            {
+                return Ok(new ResponseResult() { Data = null, Result = ResponseStatus.Success, Message = "Mission deleted successfully." });
+            }
+            return NotFound(new ResponseResult() { Data = null, Result = ResponseStatus.Error, Message = "Mission not found." });
+        }
+
+        [HttpGet]
+        [Route("MissionDetailById/{id:int}")]
+        public async Task<IActionResult> GetMissionById(int id)
+        {
+            var response = await _missionService.GetMissionById(id);
+            return Ok(new ResponseResult() { Data = response, Result = ResponseStatus.Success, Message = "" });
+        }
+    }
+}
